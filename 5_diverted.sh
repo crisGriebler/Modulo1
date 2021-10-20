8ad6fa1bdcdd1d5e7354b05dd93415647759aeb5
cristiangrb <cristian.griebler@sistemafiep.org.br>
2021-10-20 18:26:21 -0300

    Exercicío 5

--- 5_diverted.sh
+++ 5_diverted.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Contar quantos vôos precisaram ser redirecionados (Diverted).
-echo "There were $(grep 1 2006-sample.csv | sort -t, -k24,24n | cut -d ',' -f24 | wc -l) diverted flights."
+echo "There were $(grep 1 2006-sample.csv | sort -t, -k24,24n | cut -d ',' -f24 | paste -sd+ | bc) diverted flights."
