e850be32d3d960403a3050868dccb8ce0d089564
Cristian F. Griebler <91997898+crisGriebler@users.noreply.github.com>
2021-10-25 16:32:36 -0300

    update latestcia2.0

--- latestcia.sh
+++ latestcia.sh
@@ -4,13 +4,13 @@
 # arquivo com todas as cias
 tail -n +2 2006-sample.csv | cut -d ',' -f9 | sort | uniq > cias.csv # comando uniq deixa que tenha apenas uma vez cada nome
 
-# BUSCANDO NO ARQUIVO ORIGINAL PELOS NOMES DOS AEROPORTOS
+# BUSCANDO NO ARQUIVO ORIGINAL PELOS NOMES DAS CIAS
 
 filename='cias.csv'
 while read line;
 do
 cut -d ',' -f9 2006-sample.csv | grep -w $line | uniq > cia_time.csv
-cut -d ',' -f9,17 2006-sample.csv | grep -w $line | cut -d ',' -f2 | sort | grep -v '^$' | grep -v '-' | paste -sd+ | bc >> cia_time.csv
+cut -d ',' -f9,16 2006-sample.csv | grep -w $line | cut -d ',' -f2 | sort | grep -v '^$' | grep -v '-' | paste -sd+ | bc >> cia_time.csv
 paste -sd ',' cia_time.csv >> result.csv
 done < $filename
 
