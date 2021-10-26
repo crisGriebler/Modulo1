b75a579f0d1806e92a07495aa5e0e8a6f507a2f1
cristiangrb <cristian.griebler@sistemafiep.org.br>
2021-10-25 21:33:10 -0300

    desafio1 atualizado

--- latestcia.sh
+++ latestcia.sh
@@ -1,16 +1,17 @@
+
 #!/bin/bash
 
 
 # arquivo com todas as cias
 tail -n +2 2006-sample.csv | cut -d ',' -f9 | sort | uniq > cias.csv # comando uniq deixa que tenha apenas uma vez cada nome
 
-# BUSCANDO NO ARQUIVO ORIGINAL PELOS NOMES DOS AEROPORTOS
+# BUSCANDO NO ARQUIVO ORIGINAL PELOS NOMES DAS CIAS
 
 filename='cias.csv'
 while read line;
 do
 cut -d ',' -f9 2006-sample.csv | grep -w $line | uniq > cia_time.csv
-cut -d ',' -f9,17 2006-sample.csv | grep -w $line | cut -d ',' -f1 | sort | grep -v '^$' | grep -v '-' | paste -sd+ | bc >> cia_time.csv
+cut -d ',' -f9,16 2006-sample.csv | grep -w $line | cut -d ',' -f2 | sort | grep -v '^$' | grep -v '-' | paste -sd+ | bc >> cia_time.csv
 #cut -d ',' -f16 2006-sample.csv | grep -w $line | sort | grep -v '^$' | grep -v '-' | paste -sd+ | bc >> cia_time.csv
 paste -sd ',' cia_time.csv >> result.csv
 done < $filename
@@ -24,3 +25,5 @@ echo "A cia com a maior soma nos atrasos foi a $(sort -t, -k2 -n result.csv | cu
 rm cias.csv
 rm cia_time.csv
 rm result.csv
+
+
